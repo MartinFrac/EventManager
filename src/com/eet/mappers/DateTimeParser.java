81135904696c37b46bc4f0c3c41cadd1eff4ef7f
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-04-26 14:26:36 +0100

    DateTimeParser class added

---
+++ src/com/eet/mappers/DateTimeParser.java
@@ -0,0 +1,22 @@
+package com.eet.mappers;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateTimeParser {
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public static LocalDateTime fromSql(String date) {
+        LocalDateTime dateTime;
+        try {
+            dateTime = LocalDateTime.parse(date, dateTimeFormatter);
+        } catch (DateTimeParseException e) {
+            dateTime = LocalDate.parse(date, dateFormatter).atStartOfDay();
+        }
+        return dateTime;
+    }
+}
