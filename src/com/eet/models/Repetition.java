3443ef0f8f6b83d9faf9d3991accababff005179
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-04-26 14:25:19 +0100

    Repetition class added

---
+++ src/com/eet/models/Repetition.java
@@ -0,0 +1,44 @@
+package com.eet.models;
+
+public class Repetition {
+
+    private String time_of_the_day;
+    private String day_of_the_week;
+    private String day_of_the_month;
+    private String day_of_the_year;
+
+    public Repetition() {
+    }
+
+    public String getTime_of_the_day() {
+        return time_of_the_day;
+    }
+
+    public void setTime_of_the_day(String time_of_the_day) {
+        this.time_of_the_day = time_of_the_day;
+    }
+
+    public String getDay_of_the_week() {
+        return day_of_the_week;
+    }
+
+    public void setDay_of_the_week(String day_of_the_week) {
+        this.day_of_the_week = day_of_the_week;
+    }
+
+    public String getDay_of_the_month() {
+        return day_of_the_month;
+    }
+
+    public void setDay_of_the_month(String day_of_the_month) {
+        this.day_of_the_month = day_of_the_month;
+    }
+
+    public String getDay_of_the_year() {
+        return day_of_the_year;
+    }
+
+    public void setDay_of_the_year(String day_of_the_year) {
+        this.day_of_the_year = day_of_the_year;
+    }
+}
