64f30bad057695f940e9f68dd8f1400adbed2059
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-04-26 14:08:00 +0100

    Start class added

---
+++ src/com/eet/Start.java
@@ -0,0 +1,14 @@
+package com.eet;
+
+import com.eet.database.SqliteConnection;
+import com.eet.ui.SmallFrame;
+import com.eet.ui.views.LoginUI;
+
+public class Start {
+
+    public static void main(String[] args) {
+        SqliteConnection.setPath(args[0]);
+        LoginUI loginUI = new LoginUI();
+        new SmallFrame(loginUI, loginUI.getLogin());
+    }
+}
