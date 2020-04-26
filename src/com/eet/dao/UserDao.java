b56f2f1fddf62b28ee34d1e603eb7974b5aaf9cf
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-04-26 14:23:47 +0100

    UserDao class added

---
+++ src/com/eet/dao/UserDao.java
@@ -0,0 +1,28 @@
+package com.eet.dao;
+
+import com.eet.database.SqliteConnection;
+import com.eet.mappers.UserMapper;
+import com.eet.models.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    public User findById(String id) {
+        String query = "SELECT id, name, surname, password FROM USER WHERE id = ?";
+        try (Connection connection = SqliteConnection.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setString(1, id);
+            ResultSet resultSet = pstmt.executeQuery();
+            if (resultSet.next()) {
+                return UserMapper.fromSql(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
