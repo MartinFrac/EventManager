8c8c33bc3e1203063c9a2fa430dbb66bdf1ed2b7
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-04-26 14:23:29 +0100

    EventDao class added

---
+++ src/com/eet/dao/EventDao.java
@@ -0,0 +1,34 @@
+package com.eet.dao;
+
+import com.eet.database.SqliteConnection;
+import com.eet.mappers.EventMapper;
+import com.eet.models.Event;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventDao {
+
+    public List<Event> findEventsByUserId(String id) {
+        List<Event> events = new ArrayList<>();
+        String query = "SELECT e.id, e.repetition_id, r.time_of_the_day, r.day_of_the_week, r.day_of_the_month, " +
+                "r.day_of_the_year, e.type, e.title, e.description, e.date_time, e.space_limitations, e.place " +
+                "FROM EVENT AS e LEFT JOIN REPETITION AS r ON r.id = e.repetition_id " +
+                "INNER JOIN BOOKING AS b on b.event_id = e.id " +
+                "WHERE b.user_id = ?";
+        try (Connection connection = SqliteConnection.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setString(1, id);
+            ResultSet resultSet = pstmt.executeQuery();
+
+                while (resultSet.next()) {
+                    Event event = EventMapper.toJava(resultSet);
+                    events.add(event);
+                }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return events;
+    }
+}
