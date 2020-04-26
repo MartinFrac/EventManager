61f81c6bd5e2260a866db4bb73895aa946242461
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-04-26 14:27:22 +0100

    UserMapper class added

---
+++ src/com/eet/mappers/UserMapper.java
@@ -0,0 +1,18 @@
+package com.eet.mappers;
+
+import com.eet.models.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper {
+
+    public static User fromSql(ResultSet resultSet) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getString("id"));
+        user.setName(resultSet.getString("name"));
+        user.setSurname(resultSet.getString("surname"));
+        user.setPassword(resultSet.getString("password"));
+        return user;
+    }
+}
