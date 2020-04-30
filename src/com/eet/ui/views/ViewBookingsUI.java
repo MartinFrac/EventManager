35844060915bac76da9c8b4f10cf25f223ff61d4
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-05-01 00:42:46 +0100

    filter and search buttons added and filterButton action added

--- src/com/eet/ui/views/ViewBookingsUI.java
+++ src/com/eet/ui/views/ViewBookingsUI.java
@@ -19,6 +19,9 @@ public class ViewBookingsUI extends JPanel {
 	private JTextField searchByLocation;
 	private JTextField searchByDate;
 	private JTextField searchBySpaceLimit;
+	private JButton searchButton;
+	private JButton filterButton;
+	private DefaultTableModel model;
 	private EventController eventController;
 
 	public JButton getGoBack() {
@@ -34,12 +37,27 @@ public class ViewBookingsUI extends JPanel {
 		this.setPreferredSize(new Dimension(900,600));
 		this.setBackground(new Color(192,192,192));
 
+		filterButton = new JButton();
+		filterButton.setBounds(360, 425, 140, 85);
+		filterButton.setBackground(new Color(214, 217, 223));
+		filterButton.setForeground(new Color(0,0,0));
+		filterButton.setEnabled(true);
+		filterButton.setFont(new Font("sansserif", 0, 18));
+		filterButton.setText("Filter");
+		filterButton.setVisible(true);
+		filterButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+
 		goBack = new JButton();
-		goBack.setBounds(371,450,90,35);
+		goBack.setBounds(650,425,140,85);
 		goBack.setBackground(new Color(214,217,223));
 		goBack.setForeground(new Color(0,0,0));
 		goBack.setEnabled(true);
-		goBack.setFont(new Font("sansserif",0,12));
+		goBack.setFont(new Font("sansserif",0,18));
 		goBack.setText("Go back");
 		goBack.setVisible(true);
 		goBack.addActionListener(new ActionListener() {
@@ -58,7 +76,25 @@ public class ViewBookingsUI extends JPanel {
 		searchEvent.setText("Search event");
 		searchEvent.setVisible(true);
 
-		searchTitle = textFieldGenerator(54,40,392,32, "Name of the event");
+		searchButton = new JButton();
+		searchButton.setBounds(250, 40, 90, 35);
+		searchButton.setBackground(new Color(214, 217, 223));
+		searchButton.setForeground(new Color(0, 0, 0));
+		searchButton.setEnabled(true);
+		searchButton.setFont(new Font("sansserif", 0, 12));
+		searchButton.setText("Search");
+		searchButton.setVisible(true);
+		searchButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String name = searchTitle.getText();
+				Object[][] data = eventController.getBookings(ActiveUser.getUser().getId(), name);
+				updateData(data);
+				model.fireTableDataChanged();
+			}
+		});
+
+		searchTitle = textFieldGenerator(50,40,180,35, "Name of the event");
 		searchTitle.addFocusListener(new FocusListener() {
 			@Override
 			public void focusGained(FocusEvent e) {
@@ -156,13 +192,9 @@ public class ViewBookingsUI extends JPanel {
 
 		JTable table = new JTable();
 		table.setRowHeight(40);
-		DefaultTableModel model = new DefaultTableModel();
+		model = new DefaultTableModel();
 		model.setColumnIdentifiers(columnNames);
-		if(data!=null) {
-			for(int i = 0; i < data.length; i++) {
-				model.addRow(data[i]);
-			}
-		}
+		updateData(data);
 		table.setModel(model);
 		table.getColumnModel().getColumn(6).setCellRenderer(new ButtonRenderer());
 		table.getColumnModel().getColumn(6).setCellEditor(new ButtonEditor());
@@ -174,6 +206,8 @@ public class ViewBookingsUI extends JPanel {
 
 		//adding components to contentPane panel
 		this.add(goBack);
+		this.add(filterButton);
+		this.add(searchButton);
 		this.add(searchEvent);
 		this.add(searchTitle);
 		this.add(searchByKeyword);
@@ -181,6 +215,16 @@ public class ViewBookingsUI extends JPanel {
 		this.add(searchByDate);
 		this.add(searchBySpaceLimit);
 		this.add(scrollPane);
+	}
+
+	private void updateData(Object[][] data) {
+		model.getDataVector().removeAllElements();
+		if(data!=null) {
+			for(int i = 0; i < data.length; i++) {
+				model.addRow(data[i]);
+			}
+		}
+		model.fireTableDataChanged();
 	}
 
 	private static JTextField textFieldGenerator(int x, int y, int width, int height, String text) {
