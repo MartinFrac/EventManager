a7185b05998f44675819153dc9b359da4b330944
mamaj20 <martynamaj@icloud.com>
2020-05-05 18:23:16 +0100

    getting events from event dao

--- src/com/eet/controllers/EventController.java
+++ src/com/eet/controllers/EventController.java
@@ -3,6 +3,7 @@ package com.eet.controllers;
 import com.eet.dao.EventDao;
 import com.eet.models.Event;
 
+import java.util.Date;
 import java.util.List;
 
 public class EventController {
@@ -31,4 +32,48 @@ public class EventController {
             return objects;
         }
     }
+    public Object getEvents() {
+        List<Event> searchEvents = eventDao.getEventsList();
+        if (searchEvents.isEmpty()) {
+            return null;
+        }
+        else{
+            Object[][] objects = new Object[searchEvents.size()][7];
+            for (int i = 0; i < searchEvents.size(); i++) {
+                objects[i][0] = searchEvents.get(i).getTitle();
+                objects[i][1] = searchEvents.get(i).getType().toString().toLowerCase();
+                objects[i][2] = searchEvents.get(i).getDescription();
+                objects[i][3] = searchEvents.get(i).getDateTime();
+                objects[i][4] = searchEvents.get(i).getSpaceLimitations();
+                objects[i][5] = searchEvents.get(i).getPlace();
+                objects[i][6] = "Book";
+            }
+            return objects;
+        }
+
+    }
+
+    public Object cancelEvents() {
+        List<Event> searchEvents = eventDao.getEventsList();
+        if (searchEvents.isEmpty()) {
+            return null;
+        }
+        else{
+            Object[][] objects = new Object[searchEvents.size()][7];
+            for (int i = 0; i < searchEvents.size(); i++) {
+                objects[i][0] = searchEvents.get(i).getTitle();
+                objects[i][1] = searchEvents.get(i).getType().toString().toLowerCase();
+                objects[i][2] = searchEvents.get(i).getDescription();
+                objects[i][3] = searchEvents.get(i).getDateTime();
+                objects[i][4] = searchEvents.get(i).getSpaceLimitations();
+                objects[i][5] = searchEvents.get(i).getPlace();
+                objects[i][6] = "Cancel";
+            }
+            return objects;
+        }
+    }
+
+
+
+
 }
