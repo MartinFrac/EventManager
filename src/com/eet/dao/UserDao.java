12b4b2c9ab565d8606080924f8f317e489c391ad
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-05-05 03:27:58 +0100

    changed UserMapper method from fromSql to toJava

--- src/com/eet/dao/UserDao.java
+++ src/com/eet/dao/UserDao.java
@@ -18,7 +18,7 @@ public class UserDao {
             pstmt.setString(1, id);
             ResultSet resultSet = pstmt.executeQuery();
             if (resultSet.next()) {
-                return UserMapper.fromSql(resultSet);
+                return UserMapper.toJava(resultSet);
             }
         } catch (SQLException e) {
             e.printStackTrace();
