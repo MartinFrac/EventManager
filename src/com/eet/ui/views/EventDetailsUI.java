986a345c388a98847f2b2c37954d52e3a7f8cc84
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-05-21 12:50:30 +0100

    added some demo code to EventDetailsUI to make it work with the framework

--- src/com/eet/ui/views/EventDetailsUI.java
+++ src/com/eet/ui/views/EventDetailsUI.java
@@ -1,4 +1,16 @@
 package com.eet.ui.views;
 
-public class EventDetailsUI {
+import javax.swing.*;
+
+public class EventDetailsUI extends JPanel {
+
+    private JButton createButton;
+
+    public JButton getCreateButton() {
+        return createButton;
+    }
+
+    public EventDetailsUI() {
+        createButton = new JButton();
+    }
 }
