7401450f49d79366c5f58bf97f663d20e8f16990
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-05-23 19:48:11 +0100

    AdminUI implemented

--- src/com/eet/ui/views/AdminUI.java
+++ src/com/eet/ui/views/AdminUI.java
@@ -1,4 +1,78 @@
 package com.eet.ui.views;
 
-public class AdminUI {
+import com.eet.memory.ActiveUser;
+import com.eet.ui.SmallFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdminUI extends JPanel {
+
+    private JButton searchEventsButton;
+    private JButton searchStudentsButton;
+    private JButton logoutButton;
+
+    //Constructor
+    public AdminUI(){
+
+
+        //pane with null layout
+        this.setLayout(null);
+        this.setPreferredSize(new Dimension(400,500));
+        this.setBackground(new Color(139,217,169));
+
+
+        searchEventsButton = new JButton();
+        searchEventsButton.setBounds(100,90,200,35);
+        searchEventsButton.setBackground(new Color(214,72,105));
+        searchEventsButton.setForeground(new Color(51,255,255));
+        searchEventsButton.setEnabled(true);
+        searchEventsButton.setFont(new Font("SansSerif",0,20));
+        searchEventsButton.setText("Search Event");
+        searchEventsButton.setVisible(true);
+        searchEventsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        searchStudentsButton = new JButton();
+        searchStudentsButton.setBounds(100,170,200,35);
+        searchStudentsButton.setBackground(new Color(214,72,105));
+        searchStudentsButton.setForeground(new Color(51,255,255));
+        searchStudentsButton.setEnabled(true);
+        searchStudentsButton.setFont(new Font("SansSerif",0,20));
+        searchStudentsButton.setText("Search Students");
+        searchStudentsButton.setVisible(true);
+        searchStudentsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        logoutButton = new JButton();
+        logoutButton.setBounds(125,330,150,35);
+        logoutButton.setBackground(new Color(214,72,105));
+        logoutButton.setForeground(new Color(51,255,255));
+        logoutButton.setEnabled(true);
+        logoutButton.setFont(new Font("SansSerif",0,20));
+        logoutButton.setText("Logout");
+        logoutButton.setVisible(true);
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LoginUI loginUI = new LoginUI();
+                SmallFrame.getjFrame().changePanel(loginUI);
+                ActiveUser.setUser(null);
+            }
+        });
+
+        //adding components to contentPane panel
+        this.add(searchEventsButton);
+        this.add(searchStudentsButton);
+        this.add(logoutButton);
+    }
 }
