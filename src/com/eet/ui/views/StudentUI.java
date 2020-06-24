69f0f09801b24bfea7a47e3bfdfb20124ca2f675
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-06-24 17:00:50 +0100

    StudentUI refactored with ButtonBuilder

--- src/com/eet/ui/views/StudentUI.java
+++ src/com/eet/ui/views/StudentUI.java
@@ -1,8 +1,9 @@
 package com.eet.ui.views;
 
 import com.eet.memory.ActiveUser;
-import com.eet.ui.BigFrame;
-import com.eet.ui.DesignatedFrame;
+import com.eet.ui.ButtonBuilder;
+import com.eet.ui.Frame;
+import com.eet.ui.Frames;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -25,57 +26,45 @@ public class StudentUI extends JPanel {
         this.setPreferredSize(new Dimension(400,500));
         this.setBackground(new Color(139,217,169));
 
+        searchEventsButton = new ButtonBuilder("Search Events")
+                .withBounds(100, 90, 200, 35)
+                .withBackground(new Color(214,72,105))
+                .withForeground(new Color(51,255,255))
+                .withFont(new Font("SansSerif",0,20))
+                .withActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        SearchEventsUI searchEventsUI = new SearchEventsUI();
+                        Frames.getJFrame(Frame.Big).changePanel(searchEventsUI, searchEventsUI.getGoBackButton());
+                    }
+                }).build();
 
-        searchEventsButton = new JButton();
-        searchEventsButton.setBounds(100,90,200,35);
-        searchEventsButton.setBackground(new Color(214,72,105));
-        searchEventsButton.setForeground(new Color(51,255,255));
-        searchEventsButton.setEnabled(true);
-        searchEventsButton.setFont(new Font("SansSerif",0,20));
-        searchEventsButton.setText("Search Events");
-        searchEventsButton.setVisible(true);
-        searchEventsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                SearchEventsUI searchEventsUI = new SearchEventsUI();
-//                DesignatedFrame.getJFrame().dispose();
-                new BigFrame(searchEventsUI, searchEventsUI.getGoBackButton());
-            }
-        });
+        viewBookingsButton = new ButtonBuilder("View Bookings")
+                .withBounds(100, 170, 200, 35)
+                .withBackground(new Color(214,72,105))
+                .withForeground(new Color(51,255,255))
+                .withFont(new Font("SansSerif",0,20))
+                .withActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        ViewBookingsUI viewBookingsUI = new ViewBookingsUI();
+                        Frames.getJFrame(Frame.Big).changePanel(viewBookingsUI, viewBookingsUI.getGoBackButton());
+                    }
+                }).build();
 
-        viewBookingsButton = new JButton();
-        viewBookingsButton.setBounds(100,170,200,35);
-        viewBookingsButton.setBackground(new Color(214,72,105));
-        viewBookingsButton.setForeground(new Color(51,255,255));
-        viewBookingsButton.setEnabled(true);
-        viewBookingsButton.setFont(new Font("SansSerif",0,20));
-        viewBookingsButton.setText("View Bookings");
-        viewBookingsButton.setVisible(true);
-        viewBookingsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ViewBookingsUI viewBookingsUI = new ViewBookingsUI();
-//                DesignatedFrame.getJFrame().dispose();
-                new BigFrame(viewBookingsUI, viewBookingsUI.getGoBackButton());
-            }
-        });
-
-        logoutButton = new JButton();
-        logoutButton.setBounds(125,330,150,35);
-        logoutButton.setBackground(new Color(214,72,105));
-        logoutButton.setForeground(new Color(51,255,255));
-        logoutButton.setEnabled(true);
-        logoutButton.setFont(new Font("SansSerif",0,20));
-        logoutButton.setText("Logout");
-        logoutButton.setVisible(true);
-        logoutButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                LoginUI loginUI = new LoginUI();
-//                DesignatedFrame.getJFrame().changePanel(loginUI);
-                ActiveUser.setUser(null);
-            }
-        });
+        logoutButton = new ButtonBuilder("Logout")
+                .withBounds(125, 330, 150, 35)
+                .withBackground(new Color(214,72,105))
+                .withForeground(new Color(51,255,255))
+                .withFont(new Font("SansSerif",0,20))
+                .withActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        LoginUI loginUI = new LoginUI();
+                        ActiveUser.setUser(null);
+                        Frames.getJFrame(Frame.Small).changePanel(loginUI, loginUI.getLoginButton());
+                    }
+                }).build();
 
         //adding components to contentPane panel
         this.add(searchEventsButton);
